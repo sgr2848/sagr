eaab306cb89bc6a62a070e447951cead52559b57
theomgxz <sg28r8@gmail.com>
2020-06-28 14:10:16 -0400

    prod build

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 
+// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function (req, res) {
