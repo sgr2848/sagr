d04e45e9a81d1bb5811c1028e72b9ed8184af67a
theomgxz <sg28r8@gmail.com>
2020-06-28 13:05:37 -0400

    changes to jsx files with some deletion

--- src/Components/infoFile.jsx
+++ src/Components/infoFile.jsx
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
 import classes from "./css/main.module.css";
-import BarTable from "./table";
+import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom";
+import Blogs from "./blogs";
+import Player from "./songPlayer";
+import Draggable from "react-draggable";
+import Picture from "./picture";
+import oasis from "./css/table.module.css";
 
 class Info extends Component {
   state = {
     toggle: true,
     api_res: "",
+    musicThere: false,
+    picturePreview: false,
   };
   constructor(props) {
-    super(props)
-    this.state = {apiResponse:""}
+    super(props);
+    this.state = { apiResponse: "" };
   }
-  call_backend() {
-    fetch("http://localhost:9000/getDb")
-      .then((res) => res.json())
-      .then((res) => this.setState({ api_res: res.title }))
-      .catch((err) => err);
-  }
-  componentWillMount() {
-    this.call_backend();
-  }
+  // call_backend() {
+  //   fetch("http://localhost:9000/getDb")
+  //     .then((res) => res.json())
+  //     .then((res) => this.setState({ api_res: res.title }))
+  //     .catch((err) => err);
+  // }
+  // componentWillMount() {
+  //   this.call_backend();
+  // }
   render() {
+        const isMusicPlay = this.state.musicThere;
+        const isPicture = this.state.picturePreview;
     return (
       <div className={classes.info}>
         <h1 className={classes.title}>
@@ -36,13 +45,98 @@ class Info extends Component {
         </p>
         <p className={classes.newpara}>Here are some of my work</p>
         <div className={classes.wrapper}>
-          <BarTable/>
+          <div>
+            <div className={oasis.mainList}>
+              <Router>
+                <button className={oasis.draw_border}>Projects</button>
+
+                <button
+                  className={oasis.draw_border}
+                  onClick={() => {
+                    this.setState({
+                      musicThere: true,
+                    });
+                  }}
+                >
+                  Music And That Sort of Things
+                </button>
+
+                <button
+                  className={oasis.draw_border}
+                  onClick={() => {
+                    this.setState({
+                      picturePreview: true,
+                    });
+                  }}
+                >
+                  Pictures I took
+                </button>
+
+                <button className={oasis.draw_border}>
+                  <Link to="/blogs">Writings & Blogs</Link>
+                </button>
+                <Switch>
+                  <Route path="/blogs">
+                    <Blogs />
+                  </Route>
+                </Switch>
+              </Router>
+            </div>
+            <div className={oasis.Picture}>
+              {isPicture ? (
+                <div className={oasis.picture}>
+                  <button
+                    className={oasis.closeButton}
+                    onClick={() => {
+                      this.setState({
+                        picturePreview: false,
+                      });
+                    }}
+                  >
+                    x
+                  </button>
+                  <Picture />
+                </div>
+              ) : (
+                <span></span>
+              )}
+            </div>
+
+            <div className={oasis.musicPop}>
+              {isMusicPlay ? (
+                <Draggable>
+                  <div>
+                    <button
+                      className={oasis.closeButton}
+                      onClick={() => {
+                        this.setState({ musicThere: false });
+                      }}
+                    >
+                      x
+                    </button>
+                    <Player />
+                  </div>
+                </Draggable>
+              ) : (
+                <span></span>
+              )}
+            </div>
+          </div>
         </div>
 
         <p>{this.state.api_res}</p>
       </div>
     );
   }
+  player() {
+    return (
+      <div>
+        <Player />
+      </div>
+    );
+  }
 }
+
+
 
 export default Info;
