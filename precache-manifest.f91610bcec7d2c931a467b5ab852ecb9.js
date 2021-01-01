354c0b4e1e21a18413a73675f90ed049c10e0598
Sagar Shrestha <sg28r8@gmail.com>
2021-01-01 23:41:25 +0545

    Updates

--- precache-manifest.f91610bcec7d2c931a467b5ab852ecb9.js
+++ precache-manifest.f91610bcec7d2c931a467b5ab852ecb9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "30b3e13257aa1fd2e64de3cd259b52a9",
+    "revision": "816bda789fc65c8023f063edb4923d27",
     "url": "/sagr/index.html"
   },
   {
-    "revision": "fd0528a1b1a3e0b9cb6d",
+    "revision": "9622de31e37d3e6f1ad4",
     "url": "/sagr/static/css/main.8a3137c7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sagr/static/js/2.16c8045c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd0528a1b1a3e0b9cb6d",
-    "url": "/sagr/static/js/main.32f55111.chunk.js"
+    "revision": "9622de31e37d3e6f1ad4",
+    "url": "/sagr/static/js/main.63a9265c.chunk.js"
   },
   {
     "revision": "2e2c39614147a9080739",
